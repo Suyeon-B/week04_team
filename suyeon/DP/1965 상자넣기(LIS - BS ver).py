f21efc6fd23d8a242d9a5842dc8f16996a8b6cc6
Suyeon-B <tndus4243@naver.com>
2022-04-24 23:47:50 +0900

    (스터디 문제) LIS

---
+++ suyeon/DP/1965 상자넣기(LIS - BS ver).py
@@ -0,0 +1,34 @@
+# 이분탐색 풀이 ver.
+# LIS 문제와 동일
+import sys
+sys.stdin = open("DP/input.txt",'r')
+input = sys.stdin.readline
+
+def find_idx(list, k):
+    l, r = 0, len(list)-1
+    while l<r:
+        m = l+(r-l)//2
+        if list[m] < k:
+            l = m+1
+        else:
+            r = m
+    return l
+
+def LIS():
+    n = int(input().strip())
+    A = list(map(int, input().strip().split()))
+    cnt = 0
+
+    lis = [A[0]] + [1e9]*(n-1)
+
+    for i in range(1, n):
+        idx = find_idx(lis, A[i])
+        lis[idx] = A[i]
+    
+    for i in range(n):
+        if lis[i] != 1e9:
+            cnt+=1
+    
+    print(cnt)
+
+LIS()
